3afa2f5c519f342307a48cb1793c3d6b79af5f5e
Arye Eidelman <aryedov.e@gmail.com>
2019-06-02 14:35:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-function preventRefreshOnSubmit(){
-    document.querySelector('form').addEventListener('submit', function(event){
-      event.preventDefault()
-    })
+function preventRefreshOnSubmit() {
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+  })
+}
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  const employeeList = document.querySelector('.employee-list')
+  employeeList.innerHTML = employeeList.innerHTML + `<li>${retrieveEmployeeInformation()}</li>`
+}
+
+function addNewLiOnClick() {
+  addNewElementAsLi()
+  document.querySelector('input').value = ''
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', () => {
+    document.getElementsByClassName('employee-list')[0].innerHTML = ''
+  })
 }
 
 preventRefreshOnSubmit()
