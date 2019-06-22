05f62d4b6524173cc9714b6ee6dc604749515a4b
Paul Ashour <paul.ashour@gmail.com>
2019-06-22 12:07:25 -0600

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value;
+}
+
+function addNewElementAsLi() {
+  document.querySelector('ul').innerHTML = `<li>${retrieveEmployeeInformation()}</li>`
+}
+
+function addNewLiOnClick() {
+  document.addEventListener('click', addNewElementAsLi());
+  document.querySelector('input').value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', (e => document.querySelector('ul').innerHTML = ""));
+}
